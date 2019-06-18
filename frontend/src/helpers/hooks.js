363ce6352e8a8f4159f08756997213310c66963a
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2019-06-18 19:13:10 -0300

    Creates useGET hook

--- frontend/src/helpers/hooks.js
+++ frontend/src/helpers/hooks.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function useToggler(initialState) {
   const [state, setState] = useState(initialState);
@@ -10,4 +10,30 @@ export function useToggler(initialState) {
   const toggle = () => setState(!state);
 
   return [state, setTrue, setFalse, toggle];
+}
+
+export function useGET(url, options) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (loading) {
+        const rawResponse = await fetch(url, {
+          ...options,
+          method: "GET" //prevents people from being funny
+        });
+        const data = await rawResponse.json();
+        setData(data);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [loading, options, url]);
+
+  const refetch = () => {
+    setLoading(true);
+  };
+
+  return [data, loading, refetch];
 }
